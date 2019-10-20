8717c4473eed591cb21a54ed31e517803e3d08b4
ChristineH122 <104428@fhwn.ac.at>
2019-10-20 17:57:59 +0200

    added tests

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('#indexOf()', function() {
+    it('should return 1 for first vaulue', function() {
+      assert.equal([0, 2, 3].indexOf(0), 1);
+    });
+  });
+});
